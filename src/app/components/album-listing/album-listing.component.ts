f6f4ad1d4b89c26d5b2c1a355ca12ddc6851a9e2
Sam Ewaleifoh <ddmx@me.com>
2023-10-09 09:52:07 -0400

    add more components (album listing and album card)

---
+++ src/app/components/album-listing/album-listing.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ms-album-listing',
+  templateUrl: './album-listing.component.html',
+  styleUrls: ['./album-listing.component.scss']
+})
+export class AlbumListingComponent {
+
+}
